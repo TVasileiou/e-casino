86d0d15295b35e79735b597db44e52776649805a
Themis <vasiliouths@gmail.com>
2022-04-14 16:44:05 +0300

    router added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { Routes, Route, Link } from "react-router-dom";
 import LoginPage from "./components/loginPage/loginPage";
+import HomePage from "./components/homePage/homePage";
 
 function App() {
   return (
     <div className="App">
-      <LoginPage />
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="home" element={<HomePage />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/homePage/homePage.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const homePage = () => {
+  return (
+    <div className="homePage">
+      <div className="navbar">
+        <h1>here will be the navbar</h1>
+      </div>
+      <div className="mainBody">
+        <h2> here will be all the content</h2>
+      </div>
+      <div className="footer">
+        <h2> footer here</h2>
+      </div>
+    </div>
+  );
+};
+
+export default homePage;
